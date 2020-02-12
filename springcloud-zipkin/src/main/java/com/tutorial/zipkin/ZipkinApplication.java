eaa0f71db086161c3b3b4daa31cd03c019bd21e5
huangwei-github <mrhuangwe@foxmail.com>
2020-02-12 21:30:03 +0800

    -

---
+++ springcloud-zipkin/src/main/java/com/tutorial/zipkin/ZipkinApplication.java
@@ -0,0 +1,13 @@
+package com.tutorial.zipkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class ZipkinApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ZipkinApplication.class, args);
+    }
+}
