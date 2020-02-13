9b7d483ab6d644d15511d03acaeccc8809295387
huangwei-github <mrhuangwe@foxmail.com>
2020-02-13 15:53:20 +0800

    -

--- springcloud-itoken/itoken-eureka/src/main/java/com/tutorial/itoken/eureka/EurekaApplication.java
+++ springcloud-itoken/itoken-eureka/src/main/java/com/tutorial/itoken/eureka/EurekaApplication.java
@@ -2,9 +2,11 @@ package com.tutorial.itoken.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @EnableEurekaServer
 public class EurekaApplication {
     public static void main(String[] args) {
